91b20759a74ca9b0bccae22978bfe2c50236998b
Kenny Chen <kenchen10@berkeley.edu>
2020-07-15 22:44:59 -0400

    imread/write

--- sample.py
+++ sample.py
@@ -1,5 +1,12 @@
 import cv2
 
 path = 'data/'
-img = cv2.imread(path + 'lena.jpg', 0)
-print(img)
+img = cv2.imread(path + 'lena.jpg', 1)
+cv2.imshow('image', img)
+k = cv2.waitKey(0)
+
+if k == 27:
+    cv2.destroyAllWindows()
+elif k == ord('s'):
+    cv2.imwrite('lena_copy.png', img)
+    cv2.destroyAllWindows()

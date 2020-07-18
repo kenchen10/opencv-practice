1fcea0e3baff188337dd0595c5bc4564438096f9
Kenny Chen <kenchen10@berkeley.edu>
2020-07-17 22:47:45 -0400

    more mouse clicks

--- mouse.py
+++ mouse.py
@@ -5,23 +5,18 @@ import numpy as np
 # print(events)
 
 def click_event(event, x, y, flags, param):
-    font = cv2.FONT_HERSHEY_SIMPLEX
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(x, ', ', y)
-        strXY = str(x) + ', ' + str(y)
-        cv2.putText(img, strXY, (x, y), font, .5, (255, 255, 0), 2)
-        cv2.imshow('image', img)
-    if event == cv2.EVENT_RBUTTONDOWN:
         blue = img[y, x, 0]
         green = img[y, x, 1]
         red = img[y, x, 2]
-        bgr = str(blue) + ', ' + str(green) + ', ' + str(red)
-        cv2.putText(img, bgr, (x, y), font, .5, (0, 255, 255), 2)
-        cv2.imshow('image', img)
-
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        mycolorImage = np.zeros((512, 512, 3), np.uint8)
+        mycolorImage[:] = [blue, green, red]
+        cv2.imshow('color', mycolorImage)
 
 img = cv2.imread('data/lena.jpg')
 cv2.imshow('image', img)
+points = []
 
 cv2.setMouseCallback('image', click_event)
 

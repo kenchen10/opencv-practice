6f9f4cc6c31f8eeab3a376b6f901e9106a01e6af
Kenny Chen <kenchen10@berkeley.edu>
2020-07-17 22:16:17 -0400

    mouse

--- video.py
+++ video.py
@@ -1,19 +1,23 @@
 import cv2 
+import datetime
 
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20, (640, 480))
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1208)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# print(cap.get(3))
+# print(cap.get(4))
 
 while cap.isOpened():
     ret, frame = cap.read()
 
     if ret:
-        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        out.write(frame)
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("frame", gray)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = 'Width: ' + str(cap.get(3)) + ' Height: ' + str(cap.get(4))
+        datet = str(datetime.datetime.now())
+        frame = cv2.putText(frame, datet, (10, 50), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow("frame", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
